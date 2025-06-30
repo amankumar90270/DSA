ea0e434780a3e6a7a596ef616e6ff4b9301ea461
amankumar90270 <amankumar731841@gmail.com>
2025-06-30 22:36:15 +0530

    delete file

--- src/Hii/first.java
+++
@@ -1,7 +0,0 @@
-package Hii;
-
-public class first {
-    public static void main(String[] args) {
-        System.out.println("Hii This is NOrmal");
-    }
-}
--- src/Hii/second.java
+++
@@ -1,7 +0,0 @@
-package Hii;
-
-public class second {
-    public static void main(String[] args) {
-        System.out.println("This si second");
-    }
-}
