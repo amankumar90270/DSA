b49f4725173e64342fac391a6cb5bd10237f6d65
amankumar90270 <amankumar731841@gmail.com>
2025-06-30 22:33:43 +0530

    new File

---
+++ src/Hii/first.java
@@ -0,0 +1,7 @@
+package Hii;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hii This is NOrmal");
+    }
+}
---
+++ src/Hii/second.java
@@ -0,0 +1,7 @@
+package Hii;
+
+public class second {
+    public static void main(String[] args) {
+        System.out.println("This si second");
+    }
+}
