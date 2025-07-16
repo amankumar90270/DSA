b16a8c5728b14bef7baa4860c73d613b4d74ac63
Aman Kumar <amnkumar731841@gmail.com>
2025-07-17 00:13:18 +0530

    commit

---
+++ src/Loops/Pattern/Que_10.java
@@ -0,0 +1,26 @@
+//********** Print the given Pattern 1  * * * * *
+//* * * *
+//* * *
+//* *
+//*
+
+package Loops.Pattern;
+
+import java.util.Scanner;
+
+public class Que_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for (int j=1; j<=n+1-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
