3387b076117a769b74468deac4dc3f55daad7f1b
Aman Kumar <amnkumar731841@gmail.com>
2025-07-20 23:25:49 +0530

    commit

---
+++ src/Loops/Pattern/Que_18.java
@@ -0,0 +1,30 @@
+//********** Print the given Pattern  ***********
+//1
+//0 1
+//1 0 1
+//0 1 0 1
+//1 0 1 0 1
+package Loops.Pattern;
+
+import java.util.Scanner;
+
+public class Que_18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if(i%2!=0){
+                    if(j%2!=0) System.out.print(1+" ");
+                    else System.out.print(0+" ");
+                }else{
+                    if (j%2==0) System.out.print(1+" ");
+                    else System.out.print(0+" ");
+                }
+            }
+                System.out.println();
+        }
+    }
+}
