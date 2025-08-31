b835ea8f618e75844d21699fcc6ed1aa0f53cbb1
Aman Kumar <amankumar731841@gmail.com>
2025-08-31 22:36:39 +0530

    reverse array

---
+++ src/Arrays/Que_19.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Que_19 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array");
+        int n = sc.nextInt();
+
+        //input of array
+        System.out.println("Enter Elements of Array");
+        int []arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i]= sc.nextInt();
+        }
+
+
+        //Reverse of Array
+        int i=0, j=n-1;
+        while (i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+       for (int ele: arr){
+           System.out.print(ele+" ");
+       }
+    }
+}
