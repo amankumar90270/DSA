0210fdcb86cd6a48f434a7aa8faad3c26f627886
Aman Kumar <amankumar731841@gmail.com>
2025-10-04 02:16:14 +0530

    enhanced bubble sort

--- src/Arrays/Bubble_sort.java
+++ src/Arrays/Bubble_sort.java
@@ -32,7 +32,7 @@ public class Bubble_sort {
         System.out.println("Sorted Array");
 
         for (int i=0; i<n-1; i++){
-            for (int j=0; j<n-1; j++){
+            for (int j=0; j<n-1-i; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
